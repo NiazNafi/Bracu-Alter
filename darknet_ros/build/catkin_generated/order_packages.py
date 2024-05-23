a0c9b5bb0133079850f5fc9adde5c913bc0f0490
Niaz Nafi Rahman <niaznafirahman@gmail.com>
2024-05-23 06:12:39 +0600

    Delete darknet_ros directory

--- darknet_ros/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/niaz/Downloads/yolo_ros/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/niaz/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/niaz/catkin_ws/devel;/opt/ros/noetic' != '' else []
